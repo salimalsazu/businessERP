e11ebeea7769d7ffb1893e59f283577782bd7db9
Salim Al Sazu <accsalimalsazu@gmail.com>
2023-12-29 19:18:34 +0600

    [DONE] Attendance Route

---
+++ HRAdmin/client/src/app/(dashboardLayout)/leave/history/page.tsx
@@ -0,0 +1,16 @@
+import LeaveHistoryTable from "@/components/leave/history/LeaveHistoryTable";
+
+const ConveyancePage = () => {
+  return (
+    <div className="p-5 w-full">
+      {/* <div className="bg-white shadow-sm rounded-md p-2 m-2  border">
+      
+      </div> */}
+      <div className="">
+        <LeaveHistoryTable />
+      </div>
+    </div>
+  );
+};
+
+export default ConveyancePage;
