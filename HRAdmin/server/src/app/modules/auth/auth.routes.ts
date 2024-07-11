9c5f5d7cf3b167fb617fe5af0d9e7604dae760b7
Salim Al Sazu <accsalimalsazu@gmail.com>
2024-07-11 21:46:31 +0600

    [ADD] Limit

--- HRAdmin/server/src/app/modules/auth/auth.routes.ts
+++ HRAdmin/server/src/app/modules/auth/auth.routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { AuthController } from './auth.controller';
+import { AuthLoginLimiter } from '../../../shared/Limit';
 
 const router = express.Router();
 
@@ -19,7 +20,7 @@ router.post(
 //   }
 // );
 
-router.post('/login', AuthController.userLogin);
+router.post('/login', AuthLoginLimiter, AuthController.userLogin);
 
 router.post('/refresh-token', AuthController.refreshToken);
 
