49850844485cfdc3630dbb69743de8ed422069f1
Salim Al Sazu <accsalimalsazu@gmail.com>
2024-09-21 21:48:14 +0600

    [UPDATE]

--- HRAdmin/server/src/app/modules/dashboard/dashboard.service.ts
+++ HRAdmin/server/src/app/modules/dashboard/dashboard.service.ts
@@ -8,7 +8,25 @@ const getAllSummery = async () => {
       prisma.requisition.count(),
     ]);
 
-    return { transactionCount, accountCount, requisitionCount };
+    const pettyCashBalance = await prisma.account.findFirst({
+      where: {
+        accountName: 'Petty Cash',
+      },
+      select: {
+        closingBalance: true,
+      },
+    });
+
+    const bankBalance = await prisma.account.findFirst({
+      where: {
+        accountName: 'SJIBL-10144',
+      },
+      select: {
+        closingBalance: true,
+      },
+    });
+
+    return { transactionCount, accountCount, requisitionCount, pettyCashBalance, bankBalance };
   } catch (error) {
     console.error(`Failed to count Data: ${error}`);
   }
