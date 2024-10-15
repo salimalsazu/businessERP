f4f76fe54768f30e3eadda4fdf5e8a586a695f19
Salim Al Sazu <accsalimalsazu@gmail.com>
2024-10-15 17:40:32 +0600

    [UPDATE]

--- HRAdmin/server/src/app/modules/transaction/trIdAutoIncrement.ts
+++ HRAdmin/server/src/app/modules/transaction/trIdAutoIncrement.ts
@@ -11,6 +11,7 @@
 //   return accountName + formattedNumber;
 // }
 
+//Next Call
 export const generateTransactionId = () => {
   // Generate a random 6-digit number (between 100000 and 999999)
   const randomNumber = Math.floor(100000 + Math.random() * 900000);
